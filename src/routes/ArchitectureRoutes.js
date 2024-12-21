0beb26f2de2aeda0136a047eab5d1e228c60bd7c
KARAN RANA <103360966+karantec@users.noreply.github.com>
2024-12-21 07:03:25 +0530

    Update ArchitectureRoutes.js

--- src/routes/ArchitectureRoutes.js
+++ src/routes/ArchitectureRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { handleUpload, createModalData, updateModalData, deleteModalData, 
+const {
+    createArchitectureData,
+    handleMulterError,
+     updateModalData, deleteModalData, 
     getModalDataById, getModalData } = require('../controllers/ArchitectureController');
 
 // Route to create a new entry with image upload
-router.post('/upload', createModalData);
+router.post('/upload',createArchitectureData);
 
 // Route to update existing modal data
 router.put('/update/:id', updateModalData);
@@ -16,5 +19,6 @@ router.get('/modal/:id', getModalDataById);
 
 // Route to get all modal data or a specific entry by ID
 router.get('/data/:id?', getModalData);
+router.use(handleMulterError);
 
 module.exports = router;
